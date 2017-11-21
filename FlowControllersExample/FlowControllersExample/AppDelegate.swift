b951045a471314d42599756e47f40ea5642c74f4
Yani Bouchkov <Yani.Buchkov@adastragrp.com>
2017-11-21 10:33:02 +0200

    initial commit for the project

---
+++ FlowControllersExample/FlowControllersExample/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FlowControllersExample
+//
+//  Created by Yani Buchkov on 11/21/17.
+//  Copyright © 2017 Mr Bouchkov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
